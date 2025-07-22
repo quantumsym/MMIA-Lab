48f3bfda2e688b5f2b248ec9ee54b93c340169b7
freenetst <sandro@freenetst.it>
2025-07-22 22:02:53 +0200

    update

---
+++ cloned/hopfieldnetwork/hopfieldnetwork/__init__.py
@@ -0,0 +1,6 @@
+from .__about__ import __version__
+from .utils import *
+from .libary import *
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), "data")
